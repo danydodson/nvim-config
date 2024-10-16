cb2f570b90df384bf59c3949c04f816738c7de79
danydodson <danydodson@gmail.com>
2024-10-15 20:55:53 -0500

    Refactor .lua files.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,45 +4,45 @@
 -- return {}
 
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        ensure_installed = {
-          "bash",
-          "c",
-          "css",
-          "dockerfile",
-          "go",
-          "gomod",
-          "gosum",
-          "html",
-          "http",
-          "javascript",
-          "json",
-          "jsonc",
-          "lua",
-          "luadoc",
-          "markdown",
-          "nginx",
-          "ocaml",
-          "query",
-          "rust",
-          "sql",
-          "toml",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "xml",
-          "yaml",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"bash",
+					"c",
+					"css",
+					"dockerfile",
+					"go",
+					"gomod",
+					"gosum",
+					"html",
+					"http",
+					"javascript",
+					"json",
+					"jsonc",
+					"lua",
+					"luadoc",
+					"markdown",
+					"nginx",
+					"ocaml",
+					"query",
+					"rust",
+					"sql",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"xml",
+					"yaml",
+				},
+			})
+		end,
+	},
 }
