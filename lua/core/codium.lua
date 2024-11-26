0a330aa79948e0d2694f15a57673e67897d5c90b
danydodson <danydodson@gmail.com>
2024-11-26 00:27:25 -0600

    Refactor and update

--- lua/core/codium.lua
+++ lua/core/codium.lua
@@ -86,8 +86,4 @@ vim.keymap.set("n", "<C-j>", [[<cmd>call VSCodeNotify('workbench.action.navigate
 vim.keymap.set("n", "<C-k>", [[<cmd>call VSCodeNotify('workbench.action.navigateUp')<cr>]])
 vim.keymap.set("n", "-", [[<cmd>call VSCodeNotify('workbench.view.explorer')<cr>]])
 vim.keymap.set("n", "<leader>gs", [[<cmd>call VSCodeNotify('workbench.view.scm')<cr>]])
-vim.keymap.set(
-  "n",
-  "<leader>'",
-  [[<cmd>call VSCodeNotify('workbench.action.toggleEditorGroupLayout')<cr> <cmd>call VSCodeNotify('workbench.action.focusPreviousGroup')<cr>]]
-)
+vim.keymap.set("n", "<leader>'", [[<cmd>call VSCodeNotify('workbench.action.toggleEditorGroupLayout')<cr> <cmd>call VSCodeNotify('workbench.action.focusPreviousGroup')<cr>]])
